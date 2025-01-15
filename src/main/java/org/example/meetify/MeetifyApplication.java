5bf0f556cf510dbc7f669fefc50b0e6a84847a59
CrisVarlui <cvarlui614@g.educaand.es>
2025-01-15 22:50:04 +0100

    Proyecto creado

---
+++ src/main/java/org/example/meetify/MeetifyApplication.java
@@ -0,0 +1,13 @@
+package org.example.meetify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetifyApplication.class, args);
+    }
+
+}
