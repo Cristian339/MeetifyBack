d583fbe49d27ebef490f14e84b0c46108459ba3c
CrisVarlui <cvarlui614@g.educaand.es>
2025-01-15 22:50:04 +0100

    Proyecto creado

---
+++ src/test/java/org/example/meetify/MeetifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.meetify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
