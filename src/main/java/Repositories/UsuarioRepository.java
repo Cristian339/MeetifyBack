5f62eb2448e85e11a2b3f4f6c444ac4ecb9018ec
Cristian <cvargasluizaga@safareyes.es>
2025-01-16 13:17:29 +0100

    Base de datos mas entidades Creadas.

---
+++ src/main/java/Repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package Repositories;
+
+import Entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>, JpaSpecificationExecutor<Usuario> {
+
+}
