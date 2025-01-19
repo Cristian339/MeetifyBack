7b1d8441163022f70109f20690327f0a2170b70f
CrisVarlui <cvarlui614@g.educaand.es>
2025-01-19 22:17:16 +0100

    Jesus hizo cambios en perfil repository y service

--- src/main/java/Repositories/PerfilRepository.java
+++
@@ -1,9 +0,0 @@
-package Repositories;
-
-import Entities.Perfil;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface PerfilRepository extends JpaRepository<Perfil, Long>, JpaSpecificationExecutor<Perfil> {
-
-}
