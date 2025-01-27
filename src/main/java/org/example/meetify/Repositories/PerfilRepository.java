7e905a7af45cc30ce09769e07ec8d251dc5c5908
CrisVarlui <cvarlui614@g.educaand.es>
2025-01-27 23:38:08 +0100

    Falta mejorar un error que sucede en perfil.

--- src/main/java/org/example/meetify/Repositories/PerfilRepository.java
+++ src/main/java/org/example/meetify/Repositories/PerfilRepository.java
@@ -3,9 +3,12 @@ package org.example.meetify.Repositories;
 import org.example.meetify.models.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PerfilRepository extends JpaRepository<Perfil, Integer> {
 
     Optional<Perfil> findByCorreoElectronico(String correoElectronico);
+    List<Perfil> findByNombreAndApellidosAndCorreoElectronico(String nombre, String apellidos, String correoElectronico);
 }
+
