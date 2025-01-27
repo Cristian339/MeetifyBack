f631b5979c562c7bf2850bb20fea7475be0ba66b
Cristian <cvargasluizaga@safareyes.es>
2025-01-27 14:26:41 +0100

    Agregado ver mi perfil back.

--- src/main/java/org/example/meetify/Controller/AuthController.java
+++ src/main/java/org/example/meetify/Controller/AuthController.java
@@ -30,7 +30,6 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<RespuestaDTO> registro(@RequestBody LoginDTO dto){
         return service.iniciarSesion(dto);
-
     }
 
 
---
+++ src/main/java/org/example/meetify/Controller/PerfilController.java
@@ -0,0 +1,21 @@
+package org.example.meetify.Controller;
+
+import lombok.AllArgsConstructor;
+import org.example.meetify.DTO.PerfilDTO;
+import org.example.meetify.Services.PerfilService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/publicacion/perfil")
+@AllArgsConstructor
+public class PerfilController {
+    private PerfilService perfilService;
+
+    @GetMapping("/{id}")
+    public PerfilDTO obtenerPerfil(@PathVariable Integer id) {
+        return perfilService.getPerfilDTOPorId(id);
+    }
+}
