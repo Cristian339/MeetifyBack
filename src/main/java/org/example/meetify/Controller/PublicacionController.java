d23e98f5bfc521cf82c69540e63055b8ca91a1e8
Jesus <jmorenojimenez@safareyes.es>
2025-01-27 11:43:03 +0100

    todas las publicaciones

--- src/main/java/org/example/meetify/Controller/PublicacionController.java
+++ src/main/java/org/example/meetify/Controller/PublicacionController.java
@@ -1,11 +1,26 @@
 package org.example.meetify.Controller;
 
 import lombok.AllArgsConstructor;
+import org.example.meetify.DTO.PublicacionDTO;
+import org.example.meetify.Services.PublicacionService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/publicacion")
 @AllArgsConstructor
 public class PublicacionController {
+
+    private PublicacionService service;
+
+    @GetMapping("/all")
+    public List<PublicacionDTO> general(){
+        return service.getAll();
+    }
+
+
+
 }
--- src/main/java/org/example/meetify/Repositories/UsuarioRepository.java
+++ src/main/java/org/example/meetify/Repositories/UsuarioRepository.java
@@ -2,9 +2,10 @@ package org.example.meetify.Repositories;
 
 import org.example.meetify.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findTopByNombreUsuario(String nombreUsuario);
     Optional<Usuario> findTopByCorreoElectronico(String correoElectronico);
