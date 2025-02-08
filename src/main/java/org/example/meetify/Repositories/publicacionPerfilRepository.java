a424f638c121b3bababd2e4b330dd335fcabb086
zGarryxx <msevillagarrido@safareyes.es>
2025-02-08 04:40:41 +0100

    Termine seguidores y seguidos la tarea y hice el metodo para salir de una publicacion cuando un usuario se une

--- src/main/java/org/example/meetify/Repositories/publicacionPerfilRepository.java
+++ src/main/java/org/example/meetify/Repositories/publicacionPerfilRepository.java
@@ -1,11 +1,15 @@
 package org.example.meetify.Repositories;
 
+import org.example.meetify.models.Perfil;
 import org.example.meetify.models.Publicacion;
 import org.example.meetify.models.PublicacionPerfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface publicacionPerfilRepository extends JpaRepository<PublicacionPerfil, Integer> {
 
     List<PublicacionPerfil> findByPublicacion(Publicacion publicacion);
+    Optional<PublicacionPerfil> findByPerfilAndPublicacion(Perfil perfil, Publicacion publicacion);
+
 }
