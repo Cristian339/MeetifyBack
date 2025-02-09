d1e3cb7aeec94217d89074868a1c55ba44c4c527
CrisVarlui <cvarlui614@g.educaand.es>
2025-02-09 23:04:43 +0100

    Mensajeria aun sigue en proceso pero ya esta mejor pulido.

--- src/main/java/org/example/meetify/Controller/MensajeController.java
+++ src/main/java/org/example/meetify/Controller/MensajeController.java
@@ -1,23 +1,75 @@
 package org.example.meetify.Controller;
 
+import org.example.meetify.DTO.ChatDBDTO;
+import org.example.meetify.DTO.ConversacionDTO;
+import org.example.meetify.DTO.MensajeDTO;
+import org.example.meetify.DTO.RespuestaDTO;
 import org.example.meetify.Services.MensajeService;
+import org.example.meetify.Services.PerfilService;
 import org.example.meetify.models.Mensaje;
+import org.example.meetify.models.Perfil;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.AllArgsConstructor;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/mensajes")
 @AllArgsConstructor
 public class MensajeController {
 
     private final MensajeService mensajeService;
+    private final PerfilService perfilService;
 
+    @MessageMapping("/enviarMensaje")
+    @SendTo("/topic/mensajes")
+    public Mensaje enviarMensaje(Mensaje mensaje) throws Exception {
+        return mensajeService.guardarMensaje(mensaje);
+    }
+
+    @GetMapping("/room/{roomId}")
+    public List<Mensaje> obtenerMensajesPorRoomId(@PathVariable String roomId) {
+        return mensajeService.obtenerMensajesPorRoomId(roomId);
+    }
+
+    @GetMapping("/conversaciones/{idPerfil}")
+    public List<ConversacionDTO> getConversacionesByIdPerfil(@PathVariable Integer idPerfil) {
+        return mensajeService.getConverdacionesByIdPerfil(idPerfil);
+    }
+
+    @GetMapping("/conversaciones")
+    public List<ChatDBDTO> getConversaciones(@RequestParam Integer idPerfil) {
+        Perfil perfil = perfilService.getById(idPerfil);
+        return mensajeService.getConversaciones(perfil);
+    }
+
+    @GetMapping("/emisor-receptor")
+    public List<MensajeDTO> getByEmisorReceptor(@RequestParam Integer idPerfil1, @RequestParam Integer idPerfil2) {
+        return mensajeService.getByEmisorReceptor(idPerfil1, idPerfil2);
+    }
+
+    @PostMapping("/enviar")
+    public RespuestaDTO enviarMensaje(@RequestBody EnviarMensajeDTO enviarMensajeDTO, @RequestParam Integer idPerfil) {
+        Perfil perfil = perfilService.getById(idPerfil);
+        return mensajeService.enviarMensaje(perfil, enviarMensajeDTO);
+    }
+
+    @PutMapping("/editar/{mensajeId}")
+    public Mensaje editarMensaje(@PathVariable Integer mensajeId, @RequestBody Mensaje mensajeActualizado) {
+        return mensajeService.editarMensaje(mensajeId, mensajeActualizado);
+    }
+
+    @DeleteMapping("/eliminar/{mensajeId}")
+    public void eliminarMensaje(@PathVariable Integer mensajeId) {
+        mensajeService.eliminarMensaje(mensajeId);
+    }
+}
+
+
+/*
     @MessageMapping("/enviarMensaje")
     @SendTo("/topic/mensajes")
     public Mensaje enviarMensaje(Mensaje mensaje) throws Exception {
@@ -27,29 +79,5 @@ public class MensajeController {
     @GetMapping("/mensajes/{roomId}")
     public List<Mensaje> obtenerMensajesPorRoomId(@PathVariable String roomId) {
         return mensajeService.obtenerMensajesPorRoomId(roomId);
-    }
-}
-
-
-/*
-public class MensajeController {
-
-    private final MensajeService mensajeService;
-
-    @MessageMapping("/test")
-    @SendTo("/tema/test")
-
-    public Mensaje enviarMensaje( Mensaje mensaje) throws Exception {
-        return mensajeService.guardarMensaje("1", mensaje);
-    }
-    */
-/*@DestinationVariable*//*
-
-
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 }*/
